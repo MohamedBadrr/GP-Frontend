c2b0394d9995f35fa7b0071236dd7cd6bc535cb6
mohamed <badr59497@gmail.com>
2023-12-08 23:17:23 +0200

    handle conflect

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import App from "./App";
-
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from './pages/Register/Register';
