044af5252f4480a2b68889e7bae7194d0e0fd39a
Ebraheem Abdel Aziz <ebraheemzezo011@gmail.com>
2024-02-26 20:19:04 +0200

    edit spaceShip componanet

--- src/pages/Game/Components/SpaceShip.js
+++ src/pages/Game/Components/SpaceShip.js
@@ -28,6 +28,29 @@ export function SpaceShip (props) {
         })
     }
     ,[gltf])
+    useEffect(()=>{
+      if (props.action.name === "left" && props.planePosition.x === -2) {
+        // position => right move => left
+        props.setAction({...props.action , name : ""})
+        setPastPosition([0, 1]);
+        props.setPlanePosition(new Vector3(0, 1, 0));
+      } else if (props.action.name === "left" && props.planePosition.x === 0) {
+        // position => medel move => left
+        props.setAction({...props.action , name : ""})
+        props.setPlanePosition(new Vector3(2, 1, 0));
+      } else if (props.action.name === "right" && props.planePosition.x === 0) {
+        // position => medel move => right
+        props.setAction({...props.action , name : ""})
+        props.setPlanePosition(new Vector3(-2, 1, 0));
+      } else if (props.action.name === "right" && props.planePosition.x === 2) {
+        // position => left move => right
+        props.setAction({...props.action , name : ""})
+        setPastPosition([1, 0]);
+        props.setPlanePosition(new Vector3(0, 1, 0));
+      } else {
+        props.setAction({...props.action , name : ""})
+      }
+    },[props.action.name , props.planePosition.x])
 
         useFrame((state,delta)=>{
         //   // edit position depend on 
@@ -53,21 +76,7 @@ export function SpaceShip (props) {
         //     } 
         //   }
           
-          if (props.action.name === "left" && props.planePosition.x === -2) {
-            // position => right move => left
-            setPastPosition([0, 1]);
-            props.setPlanePosition(new Vector3(0, 1, 0));
-          } else if (props.action.name === "left" && props.planePosition.x === 0) {
-            // position => medel move => left
-            props.setPlanePosition(new Vector3(2, 1, 0));
-          } else if (props.action.name === "right" && props.planePosition.x === 0) {
-            // position => medel move => right
-            props.setPlanePosition(new Vector3(-2, 1, 0));
-          } else if (props.action.name === "right" && props.planePosition.x === 2) {
-            // position => left move => right
-            setPastPosition([1, 0]);
-            props.setPlanePosition(new Vector3(0, 1, 0));
-          } 
+          
           
           
 
