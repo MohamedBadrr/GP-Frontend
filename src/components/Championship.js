9c5152598b18aae383c1e9cd84690afb063ace4c
Ebraheem Abdel Aziz <ebraheemzezo011@gmail.com>
2024-03-09 00:02:51 +0200

    .

--- src/components/Championship.js
+++ src/components/Championship.js
@@ -7,6 +7,8 @@ import "../pages/RPS-game/Round.css";
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { getAuthUser } from "../helper/Storage";
+const auth = getAuthUser()
 const Championship = (props) => {
   const navigate = useNavigate();
   return (
@@ -16,13 +18,13 @@ const Championship = (props) => {
     <button onClick={()=>{navigate('/game-round/100')}} style={{marginBottom: '10px'}}>Start Championship 2 (Costs 200 coins)</button>
     <button onClick={()=>{navigate('/game-round/300')}} style={{marginBottom: '10px'}}>Start Championship 3 (Costs 400 coins)</button>
      */}
-   
     <Card style={{ width: '18rem', margin:'10px' }}>
 <Card.Img variant="top" sizes="" src={props.photo} />
 <Card.Body>
   <Card.Title>{props.name} Costs {props.cost}</Card.Title>
-  
-  <Button className="championbutton" variant="primary"onClick={()=>{navigate('/game-round/'+props.id)}}>Start Championship</Button>
+  {
+    ( props.cost <= auth.coins ) &&   <Button className="championbutton" variant="primary"onClick={()=>{navigate('/game-round/'+props.id)}}>Start Championship</Button>
+  }
 </Card.Body>
 </Card>
 
