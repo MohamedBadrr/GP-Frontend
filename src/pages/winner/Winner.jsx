71aef0a1548f9983b4e31940a08e47c302835ca8
mohamed <badr59497@gmail.com>
2024-04-29 01:16:02 +0300

    handle conflect in winner

--- src/pages/winner/Winner.jsx
+++ src/pages/winner/Winner.jsx
@@ -22,35 +22,37 @@ const Winner = () => {
     top: "-9999px",
   };
 
-  
   return (
     <>
-        <div className='winner'>
+      <div className="winner">
         <div className="container-winner-game">
-            <div className='winner-titles'>
-                {/* <h2 className='winnernumbers'></h2> */}
-                <h2>
-                <audio style={audioStyle} autoPlay loop >
-                  <source src={soundFile} type="audio/mpeg" />
-                </audio>
-                <i class="fa-solid fa-trophy mx-4"></i>
-                  <span>
-                  <Typewriter
-                      words={['YOU ARE Winner...!']}
-                      loop={false}
-                      cursor
-                      cursorStyle=","
-                      typeSpeed={90}
-                      deleteSpeed={0}
-                      delaySpeed={150000}
-                      
-                    /></span>
-                  <i class="fa-solid fa-trophy mx-4"></i>
-                </h2>
-                <img src={imgWinner} alt="" className='asd'/>
-                <Link to={"/selectskin"}><h3>play again?</h3></Link>
-                <Link to={"/home"}><h3>back to main menu?</h3></Link>
-            </div>
+          <div className="winner-titles">
+            <h2>
+              <audio style={audioStyle} autoPlay loop>
+                <source src={soundFile} type="audio/mpeg" />
+              </audio>
+              <i class="fa-solid fa-trophy mx-4"></i>
+              <span>
+                <Typewriter
+                  words={["YOU ARE Winner...!"]}
+                  loop={false}
+                  cursor
+                  cursorStyle=","
+                  typeSpeed={90}
+                  deleteSpeed={0}
+                  delaySpeed={150000}
+                />
+              </span>
+              <i class="fa-solid fa-trophy mx-4"></i>
+            </h2>
+            <img src={imgWinner} alt="" />
+            <Link to={"/selectskin"}>
+              <h3>play again?</h3>
+            </Link>
+            <Link to={"/mainamenu"}>
+              <h3>back to main menu?</h3>
+            </Link>
+          </div>
         </div>
       </div>
     </>
